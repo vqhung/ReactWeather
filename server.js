851e411dcdc0a37f1eb0cb84a11644e72c1fc3b7
Hung Vo <vqhung@gmail.com>
2016-12-22 11:18:23 +0700

    Fix the port variable

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express');
 // Create our App
 
 var app = express();
-  const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(function (request, response, next) {
   if (request.headers['x-forwarded-proto'] === 'http') {
@@ -17,5 +18,5 @@ app.use(express.static('public'));
 
 app.listen(PORT, function () {
 
-  console.log(`Express server is up on port ${port}`);
+  console.log('Express server is up on port' + PORT);
 });
